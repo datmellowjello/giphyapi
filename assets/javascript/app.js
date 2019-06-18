1531b0974bd72ef0901b1794662c98d1079e55e9
davidbridges123 <david.bridges@live.com>
2019-06-18 13:13:12 -0600

    adding btn to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,9 @@
 var topics = ["jeep", "Hummer", "farrari", "bently", "porshe", "audi", "bugatti", "rollsroyce", "bmw", "tesla"]
+
+for (i=0; i < topics.length; i++) {
+    
+    $("#btnarray").append("<button>" + topics[i] + "</button>")
+
+
+
+}

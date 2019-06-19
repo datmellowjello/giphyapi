346e7db555d06956d01e36acb6ac3f6238d1f852
davidbridges123 <david.bridges@live.com>
2019-06-19 15:01:37 -0600

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,21 @@
+$(document).ready(function() {
+
 var topics = ["jeep", "Hummer", "farrari", "bently", "porshe", "audi", "bugatti", "rollsroyce", "bmw", "tesla"]
 
+function displayButtons () {
 for (i=0; i < topics.length; i++) {
+    var newBtn = $("<button><button>")
     
-    $("#btnarray").append("<button>" + topics[i] + "</button>")
-
-
-
+    // $("#btnarray").append("<button>" + topics[i] +" </button>");
+   // $("button").attr("data-car="topics[i]"")
+   $("#btnarray").text("topics[i]");
+   
 }
+$("button").on("click", function() {
+
+})
+}
+
+
+
+});

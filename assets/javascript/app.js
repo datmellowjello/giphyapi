5ca8760943d87fab0f2032823a08028dd2581d56
davidbridges123 <david.bridges@live.com>
2019-06-19 15:10:41 -0600

    adding ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var topics = ["jeep", "Hummer", "farrari", "bently", "porshe", "audi", "bugatti", "rollsroyce", "bmw", "tesla"]
+   var topics = ["jeep", "Hummer", "farrari", "bently", "porshe", "audi", "bugatti", "rollsroyce", "bmw", "tesla"]
 
-function displayButtons () {
-for (i=0; i < topics.length; i++) {
-    var newBtn = $('<button type="button">')
-    
-    // $("#btnarray").append("<button>" + topics[i] +" </button>");
-   // $("button").attr("data-car="topics[i]"")
-   newBtn.attr('data-name', topics[i]);
-   newBtn.text(topics[i]);
-   $("#btnarray").append(newBtn);
+   function displayButtons() {
+      for (i = 0; i < topics.length; i++) {
+         var newBtn = $('<button type="button">')
+
+         // $("#btnarray").append("<button>" + topics[i] +" </button>");
+         // $("button").attr("data-car="topics[i]"")
+         newBtn.attr('data-name', topics[i]);
+         newBtn.text(topics[i]);
+         $("#btnarray").append(newBtn);
+
+      }
+      $("button").on("click", function() {
+         var image = $(this).attr("data-name");
    
-}
-$("button").on("click", function() {
+         // Constructing a URL to search Giphy for the nam
+         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+           image + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+   
+         // Performing our AJAX GET request
+         $.ajax({
+           url: queryURL,
+           method: "GET"
+         })
+           // After the data comes back from the API
+           .then(function(response) {
 
-})
-}
+            
 
+
+      });
+   });
+   displayButtons();
 
 
 });

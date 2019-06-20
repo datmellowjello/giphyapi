188f87f9e01d88430ed8e0e5b575bc264c4df65e
davidbridges123 <david.bridges@live.com>
2019-06-19 18:58:54 -0600

    adding still gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,46 +11,68 @@ $(document).ready(function () {
          // $("#btnarray").append("<button>" + topics[i] +" </button>");
          // $("button").attr("data-car="topics[i]"")
          newBtn.attr('data-name', topics[i]);
+         newBtn.addClass("button1")
          newBtn.text(topics[i]);
          $("#btnarray").append(newBtn);
       };
-      };
+   };
+   displayButtons ();
 
-      $("#submitbtn").on("click", function() {
+   $("#submitbtn").on("click", function(event) {
 
-         event.preventDefault();
+      event.preventDefault();
 
-         var newtopic = $("#newadd").val().trim();
-         topics.push(newtopic);
+      var newtopic = $("#newadd").val().trim();
+      topics.push(newtopic);
 
-         displayButtons();
+      displayButtons();
 
 
-      });
-      $(document).on("click", function() {
-         var image = $(this).attr("data-name");
-   
-         // Constructing a URL to search Giphy for the nam
-         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-           image + "&api_key=71clL2OHtaJ5wO26bXXxDMXMqiC44LE8";
-   
-         // Performing our AJAX GET request
-         $.ajax({
-           url: queryURL,
-           method: "GET"
-         })
-           // After the data comes back from the API
-           .then(function(response) {
+   });
+   $('.button1').on("click", function () {
+      var image = $(this).attr("data-name");
+      var apikey = "&api_key=71clL2OHtaJ5wO26bXXxDMXMqiC44LE8"
+      // Constructing a URL to search Giphy for the nam
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+         image + apikey;
+      console.log(queryURL);
+      // Performing our AJAX GET request
+      $.ajax({
+         url: queryURL,
+         method: "GET"
+      })
+         // After the data comes back from the API
+         .then(function (response) {
             var results = response.data;
             console.log(response)
-            console.log(results);
+            for (var i = 0; i < results.length; i++) {
+               console.log(results);
+               console.log(queryURL);
+               var gifDiv = $("<div>");
+
+               var rating = results[i].rating;
+
+               var p = $("<p>").text("Rating: " + rating);
+
+               var movinggif = $("<img>");
+               var stillgif = $("<img>")
 
 
+               movinggif.attr("src", results[i].images.fixed_height.url);
+               stillgif.attr("src", results[i].images.original_still.url);
+
+               gifDiv.append(stillgif);
+               gifDiv.append(p);
+               gifDiv.append(movinggif);
+
+               $(".imghere").prepend(gifDiv);
+
+            }
 
 
-      });
+         });
+         
    });
-   displayButtons();
 
 
 });
